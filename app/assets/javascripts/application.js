2a39691862c132cb0675f0cb38fe69433f0c8747
Zach Briggs <briggszj@gmail.com>
2016-04-25 20:57:38 -0500

    Boot vue app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require material
+//= require vue
 //= require_tree .
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,12 @@
+;(function () {
+  "use strict"
+
+  $(document).ready(function () {
+    new Vue({
+      el: "body",
+
+      data: {}
+    })
+  })
+
+})();
